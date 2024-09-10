6e0fce20c0ab5a1add29e5932300e1cecee0f0d9
SeongHyun154 <seonghun154@gmail.com>
2024-09-10 16:50:08 +0900

    cfvfdf

---
+++ src/C00/C00HELLOWORLD.java
@@ -0,0 +1,11 @@
+package C00;
+
+import java.util.Iterator;
+
+public class C00HELLOWORLD {
+	public static void main(String[] args) {
+		System.out.println("HELLO WORLD");
+		
+	}
+
+}
---
+++ src/C00/test11.java
@@ -0,0 +1,7 @@
+package C00;
+
+public class test11 {
+	public static void main(String[] args) {
+		System.out.println("HELLO 2222222");
+	}
+}
